70bb5a90fe2d35c4a020814e7b655658ee0f221e
BlackMoFan <rodlester.moreno23@gmail.com>
2023-03-11 18:37:52 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Data-mining-with-flask
---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, jsonify, Response
+#import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
+
+@app.route('/ML/')
+def ML():
+    return render_template('ML.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    select_ = request.form.get('pos_1v1')
+    print(select_)
+    print('form val: ', request.form.values())
+    exp_ = request.form.get('experience')
+
+    int_features = [select_, exp_] 
+    final_features = [np.array(int_features)]
+
+    prediction = model.predict(final_features)
+
+    res = prediction.item()
+
+    return render_template('ML.html', prediction_text='Expected Salary Rate should be $ {}'.format(res))
+
+#@app.route('/predict', methods=['POST'])
+#def predict():
+#
+#    int_features = [int(x) for x in request.form.values()]
+#    final_features = [np.array(int_features)]
+#
+#    prediction = model.predict(final_features)
+#
+#    res = prediction.item()
+#
+#    return render_template('ML.html', prediction_text='Expected Salary Rate should be $ {}'.format(res))
+
+@app.route('/Act0LoadData/')
+def activity0():
+    return render_template('Activity0.html')
+
+@app.route('/Act1KNN/')
+def activity1():
+    return render_template('Activity1.html')
+
+@app.route('/Act2KMeansClustering/')
+def activity2():
+    return render_template('Activity2.html')
+
+@app.route('/Act3Association/')
+def activity3():
+    return render_template('Activity3.html')
+
+@app.route('/Act4Nbayes/')
+def activity4():
+    return render_template('Activity4.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
